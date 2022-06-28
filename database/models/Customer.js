21ad1d0e27b2f58866b669a8a1f4816e42019bbc
Matthew Villarica <matthewvillarica@gmail.com>
2022-06-28 11:44:32 +0800

    customer schema

---
+++ database/models/Customer.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const CustomerSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+     
+    },
+    lastname: {
+        type: String,
+        required: true
+      
+    },
+    //both email and pnumber should be unique to each customer
+    email: {
+        type: Number,
+        required: true
+        
+    },
+    pnumber: {
+        type: String,
+        required: true
+        
+    },
+
+    pw :{
+        type:String,
+        required: true
+    },
+
+    //Array of Drinks, for rendering of favorte drinks of customer in Favorites Page
+    //[0..*] at any time can have 0 to many drinks 
+    favoritedrinks : [{
+        type: Schema.Types.ObjectId,
+        ref: 'Drink'
+
+    }]
+});
+
+const Customer = mongoose.model('Customer', CustomerSchema);
+
+module.exports = Customer;
