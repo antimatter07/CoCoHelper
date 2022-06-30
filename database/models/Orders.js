2d2659a3a261e6c58a2f635c7d2660787d791a0d
John Carlo Joyo <john_carlo_joyo@dlsu.edu.ph>
2022-06-30 12:57:50 +0800

    added orders schema

---
+++ database/models/Orders.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var OrderSchema = new mongoose.Schema({
+
+    pnumber: {
+        type: Number,
+        required: true
+    },
+
+    orderno: Number,
+    orderdate: Date,
+    itemname: String,
+    quantity: Number,
+    amountdue: mongoose.Types.Decimal128,
+
+    status: {
+        type: String,
+        enum: ['Ready', 'In Progress']
+    }
+});
+
+module.exports = mongoose.model('Orders', OrderSchema);
