3ae7fed2254142c161f6f59f24a9d25d4263676d
Matthew Villarica <matthewvillarica@gmail.com>
2022-07-02 13:34:44 +0800

    entry id virtual

--- database/models/Entry.js
+++ database/models/Entry.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 //model for an entry in the shopping cart
 
 const EntrySchema = new mongoose.Schema({
+    
     drinkname: {
         type: String
      
@@ -39,8 +40,21 @@ const EntrySchema = new mongoose.Schema({
         type: String,
         enum : ['more', 'normal', 'less', 'none']
     }
+}
+
+    , {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+      }
     
-}); // JSON format, consisting of the name: type collection
+); // JSON format, consisting of the name: type collection
+
+
+//when a doc is made, automaticall make object ID a field in the docu as well
+//needed for deletion, updating in Shopping Cart page
+EntrySchema.virtual('id', {
+    id: this.id
+});
 
 const Entry = mongoose.model('Entry', EntrySchema);
 
