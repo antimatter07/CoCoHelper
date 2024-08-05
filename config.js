908d9aac0201439c9d045d6cf710b8363ec40210
JG8203 <joshua_gilo@dlsu.edu.ph>
2024-08-06 00:15:24 +0800

    prolly a bad idea to expose uri lmao

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const mongoURI = 'mongodb://127.0.0.1:27017/CoCoDB';
+const mongoURI = 'mongodb+srv://CSSECDV:9lw6BmVXdXRzOMkp@cluster0.pcgknvh.mongodb.net/';
 const logLevel = 'info';
 const logtailKey = 'Pu8AWofENjCUPZvh8eCssmf1'
 module.exports = {
